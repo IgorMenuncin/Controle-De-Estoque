8685cce372b4ac5ba88ca00c6df447d1859a349c
Igor Menuncin <igormenuncin@gmail.com>
2023-02-02 21:02:47 -0300

    Adicionando função de escolha de botao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,8 @@ import ListaDeProdutos from './Components/ListaDeProdutos';
 import { CadastrarProduto } from './Components/CadastrarProduto/CadastrarProduto';
 
 function App() {
+  const [MenuOption, setMenuOption] = React.useState(false);
   const [Produtos, setProdutos] = React.useState([]);
-  const [BotaoExibir, setBotaoExibir] = React.useState(false);
-  const [BotaoCadastrar, setBotaoCadastrar] = React.useState(false);
-  // const [BotaoDeletar, setBotaoDeletar] = React.useState(false);
 
   React.useEffect(() => {
     getBanco();
@@ -18,19 +16,22 @@ function App() {
     <>
       <button
         onClick={() => {
-          setBotaoCadastrar(!BotaoCadastrar)
+          MenuOption ? (MenuOption === "BotaoCadastrarProduto" ? setMenuOption(false) : setMenuOption("BotaoCadastrarProduto")) : setMenuOption("BotaoCadastrarProduto") //Função para verificar se a pagina ja esta exibindo algo
         }}
         type="button"
         className="btn btn-primary m-1">Cadastrar produto
       </button>
       <button
         onClick={() => {
-          setBotaoExibir(!BotaoExibir)
+          MenuOption ? (MenuOption === "BotaoExibirProduto" ? setMenuOption(false) : setMenuOption("BotaoExibirProduto")) : setMenuOption("BotaoExibirProduto") //Função para verificar se a pagina ja esta exibindo algo
         }}
         type="button"
         className="btn btn-primary m-1">Exibir produtos
       </button>
       <button 
+        onClick={() => {
+          MenuOption ? (MenuOption === "BotaoDeletarProduto" ? setMenuOption(false) : setMenuOption("BotaoDeletarProduto")) : setMenuOption("BotaoDeletarProduto")
+        }}
         type="button" 
         className="btn btn-primary m-1">Deletar produto</button>
       <button
@@ -38,8 +39,8 @@ function App() {
         type="button"
         className="btn btn-primary m-1">Atualizar
       </button>
-      {BotaoExibir ? <ListaDeProdutos listaDeProdutos={Produtos} /> : ""} 
-      {BotaoCadastrar ? <CadastrarProduto /> : ""}
+      {MenuOption === "BotaoCadastrarProduto" ? <CadastrarProduto /> : ""}
+      {MenuOption === "BotaoExibirProduto" ? <ListaDeProdutos listaDeProdutos={Produtos} /> : ""}
     </>
   );
 
@@ -47,6 +48,7 @@ function App() {
     const response = await axios.get("http://localhost:3223/read");
     setProdutos(response.data);
   }
+
 }
 
 export default App;
